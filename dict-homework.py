641fa61700526e5eebcb0fa0f7f9338b0d017c70
Doniyor <dyusupovopenai@gmail.com>
2024-01-11 16:00:30 +0500

    Create dict-homework.py

---
+++ dict-homework.py
@@ -0,0 +1,7 @@
+talaba1 = {'ism':'Axror','kurs':4,'fakultet':'gim'}
+talaba2 = {'ism':'Axror','kurs':3,'fakultet':'gm'}
+talaba2 = {'ism':'Axror','kurs':2,'fakultet':'gm'}
+
+print(f"{talaba1['ism']}, {talaba1['kurs']}kursda oqiydi")
+
+
